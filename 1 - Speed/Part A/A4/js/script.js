df6bf234ed902e54cb60a1c39a1cccba4dad62b4
gherkins05 <finlaysperkins@gmail.com>
2024-10-17 18:13:01 +0100

    Completed all easy questions

--- 1 - Speed/Part A/A4/js/script.js
+++ 1 - Speed/Part A/A4/js/script.js
@@ -1,1 +1,23 @@
 //Put your code here
+const smiley = document.getElementById('smiley');
+function happy() {
+    smiley.classList.add('happy');
+    smiley.classList.remove('sad');
+    smiley.classList.remove('angry');
+
+    smiley.innerHTML = ':)';
+}
+function middle() {
+    smiley.classList.add('middle');
+    smiley.classList.remove('sad');
+    smiley.classList.remove('happy');
+
+    smiley.innerHTML = ':|';
+}
+function sad() {
+    smiley.classList.add('sad');
+    smiley.classList.remove('happy');
+    smiley.classList.remove('middle');
+
+    smiley.innerHTML = ':(';
+}
